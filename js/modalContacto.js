29af5370385534f8ff35fa52310414b5baf9482b
FernandoSullca <42738690+FernandoSullca@users.noreply.github.com>
2022-12-19 20:23:43 -0300

    pagina contacto diseño

--- js/modalContacto.js
+++ js/modalContacto.js
@@ -1,11 +1,11 @@
-const open = document.getElementById('Modal');
-// const open = document.getElementById('btnSend');
+// const open = document.getElementById('Modal');
+const open = document.getElementById('btnSend');
 const modal_container = document.getElementById('modal_container');
 const close = document.getElementById('btnSendAgain');
 
-open.addEventListener('onclick', () => {
-  modal_container.classList.add('show');  
-});
+// open.addEventListener('onclick', () => {
+//   modal_container.classList.add('show');  
+// });
 
 close.addEventListener('click', () => {
   modal_container.classList.remove('show');
--- js/scriptValidarContacto.js
+++ js/scriptValidarContacto.js
@@ -1,58 +1,86 @@
-$(document).ready(function(){
+$(document).ready(function validarFormulario() {
 
-    $('#btnSend').click(function(){
+    $('#btnSend').click(function () {
 
         var errores = '';
 
         // Validado Nombre ==============================
-        if( $('#names').val() == '' ){
+        if ($('#names').val() == '') {
             errores += '<p>Escriba un Nombre</p>';
             $('#names').css("border-bottom-color", "#F14B4B")
-        } else{
+        } else {
             $('#names').css("border-bottom-color", "#d1d1d1")
         }
 
         // Validado Apellido ==============================
-        if( $('#nickname').val() == '' ){
+        if ($('#nickname').val() == '') {
             errores += '<p>Escriba un Apellido</p>';
             $('#nickname').css("border-bottom-color", "#F14B4B")
-        } else{
+        } else {
             $('#nickname').css("border-bottom-color", "#d1d1d1")
         }
 
         // Validado Correo ==============================
-        if( $('#email').val() == '' ){
+        if ($('#email').val() == '') {
             errores += '<p>Ingrese un correo</p>';
             $('#email').css("border-bottom-color", "#F14B4B")
-        } else{
+        } else {
             $('#email').css("border-bottom-color", "#d1d1d1")
         }
 
         // Validado Mensaje ==============================
-        if( $('#mensaje').val() == '' ){
+        if ($('#mensaje').val() == '') {
             errores += '<p>Escriba un mensaje</p>';
             $('#mensaje').css("border-bottom-color", "#F14B4B")
-        } else{
+        } else {
             $('#mensaje').css("border-bottom-color", "#d1d1d1")
         }
 
         // ENVIANDO MENSAJE ============================
-        if( errores == '' == false){
-            var mensajeModal = '<div class="modal_wrap">'+
-                                    '<div class="mensaje_modal">'+
-                                        '<h3>Errores encontrados</h3>'+
-                                        errores+
-                                        '<span id="btnClose">Cerrar</span>'+
-                                    '</div>'+
-                                '</div>'
+        if (errores != '') {
+            var mensajeModal = '<div class="modal-wrap">' +
+                '<div class=" mensaje_modal">' +
+                '<h3>Errores encontrados</h3>' +
+                errores +
+                '<span id="btnClose">Cerrar</span>' +
+                '</div>' +
+                '</div>'
 
             $('body').append(mensajeModal);
+
+            // CERRANDO MODAL ==============================
+
+            $('#btnClose').click(function () {
+                $('.modal-wrap').remove();
+       
+            });
+            return false;
+        } else {
+                var mensajeCorrectoModal = '<div id="modal_container" class="Modal_enviado show">' +
+                '<div class="Contenedor_modal">' +
+                '<h1>"El mensaje ha sido enviado correctamente!"</h1>' +
+                // '<span id="btnSendAgain">Cerrar</span>' +
+                // '<input type="submit" value="Enviar nuevo mensaje" id="btnSendAgain">' +
+                '</div>' +
+                '</div>'
+
+                $('body').append(mensajeCorrectoModal);
+                
+                $('#btnSendAgain').click(function () {   
+                    // $('#modal_container').remove();  
+                    // $('.Modal_enviado ').remove();  
+                 $('.Modal_enviado .show').remove(); 
+                
+                });
+                // $('#btnSendAgain').click(function () {
+                //     // $('#modal_container').remove();  
+                //     return true;
+                // });  
+                   return true;
+                // return false;
         }
 
-        // CERRANDO MODAL ==============================
-        $('#btnClose').click(function(){
-            $('.modal_wrap').remove();
-        });
     });
+
 
 });
